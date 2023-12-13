9aa99df65f7d6d98016918b474cc38a20fae170b
IrimiaMarco <irimiamarco00@gmail.com>
2023-12-12 22:54:41 -0700

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaCsharp
+{
+    internal class Car
+    {
+        public string color;
+        public string name;
+        public int year;
+        public int maxSpeed;
+        public Car(string nameN, string colorN, int yearN, int maxN)
+        {
+            name = nameN;
+            color = colorN;
+            year = yearN;
+            maxSpeed = maxN;
+
+        }
+
+        public Car(string nameN)
+        {
+            name = nameN;
+        }
+
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + " is going at the speed of " + maxSpeed);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+            Car BMW = new Car("EX5");
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.year);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.maxSpeed);
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.maxSpeed);
+
+            Console.WriteLine(BMW.name);
+            Console.ReadKey();
+        }
+    }
+}
